ace358584322fc319a0f827df7a18a8d3182dd23
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2020-03-25 17:06:02 -0400

    added new project

--- src/js/tilt.js
+++ src/js/tilt.js
@@ -3,11 +3,11 @@ import tilt from 'tilt.js';
 const initTilt = () => {
   // Projects images
   $('.project-wrapper__image a div').tilt({
-    maxTilt: 2,
+    maxTilt: 0,
   });
   // About me image
 	$('#profpic').tilt({
-		maxTilt: 3,
+		maxTilt: 0,
 	});
 };
 
